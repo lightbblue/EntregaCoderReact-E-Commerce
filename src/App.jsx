e03b798dc6c85c0247b7811f20e8c6ec276171f2
lightbblue <jstachioni@gmail.com>
2026-01-30 18:39:51 -0300

    Fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/EntregaCoderReact-E-Commerce">
+    <BrowserRouter>
       <div className="font-sans bg-fruit-100 min-h-screen">
         <NavBar />
         <Routes>
